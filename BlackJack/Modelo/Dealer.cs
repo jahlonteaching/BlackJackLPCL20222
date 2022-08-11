6485d92fa4b20cd5077b594c1eb665477bacb56e
LeoUribC <leo.uribe1205@gmail.com>
2022-08-11 15:43:23 -0500

    Se agregó la clase Dealer

---
+++ BlackJack/Modelo/Dealer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Modelo
+{
+    class Dealer
+    {
+        public int Ganados { get; set; }
+        public int Perdidos { get; set; }
+
+        public Mano Mano { get; set; }
+        public Baraja Baraja { get; set; }
+        public Dealer()
+        {
+            Mano = new Mano();
+            Ganados = 0;
+            Perdidos = 0;
+            Baraja = new Baraja();
+        }
+
+        public Carta RepartirCarta()
+        {
+            return Baraja.Repartir();
+        }
+
+        public void RecibirCarta(Carta carta)
+        {
+            Mano.AgregarCarta(carta);
+        }
+    }
+}
