7ca3c872d15489fdbd77bc23b88edfd3a7ea7e65
Kalichhe <99373990+Kalichhe@users.noreply.github.com>
2022-08-11 15:22:12 -0500

    Se creo una nueva clase llamada Jugador

---
+++ BlackJack/Modelo/Jugador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Modelo
+{
+    class Jugador
+    {
+        public string Nombre { get; }
+        public int Ganados { get; set; }
+        public int Perdidos { get; set; }
+        public Mano Mano { get; set; }
+
+        public Jugador(string nombre)
+        {
+            this.Nombre = nombre;
+            Ganados = 0;
+            Perdidos = 0;
+            Mano = new Mano();
+
+        }
+
+        public void RecibirCarta(Carta carta)
+        {
+            Mano.AgregarCarta(carta);
+
+        }
+    }
+}
