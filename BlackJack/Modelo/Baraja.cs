7dd69899e2b4b6b0becb026833242ea781fdead0
dwn84 <50523550+dwn84@users.noreply.github.com>
2022-08-11 14:27:18 -0500

    Se agregaron la versión inicial de las clases Baraja y Carta

---
+++ BlackJack/Modelo/Baraja.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Modelo
+{
+    class Baraja
+    {
+        private List<Carta> cartas;
+
+        public Baraja()
+        {
+            cartas = new List<Carta>();
+            InicializarCartas();
+        }
+
+        private void InicializarCartas()
+        {
+            foreach (Pintas pinta in Enum.GetValues(typeof(Pintas)))
+            {
+                foreach (Valores valor in Enum.GetValues(typeof(Valores)))
+                {
+                    cartas.Add(new Carta(valor, pinta));
+                }
+            }
+        }
+
+        public void Revolver()
+        {
+            Random rand = new Random();
+            cartas = cartas.OrderBy(c => rand.Next()).ToList();
+        }
+
+        public Carta Repartir()
+        {
+            if (cartas.Count > 0)
+            {
+                Carta carta = cartas[0];
+                cartas.RemoveAt(0);
+                return carta;
+            }
+            else
+                return null;
+        }
+
+    }
+}
---
+++ BlackJack/Modelo/Carta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Modelo
+{
+    public enum Pintas
+    {
+        Corazones,
+        Diamantes,
+        Treboles,
+        Espadas
+    }
+
+    public enum Valores
+    {
+        A = 1,
+        DOS = 2,
+        TRES = 3,
+        CUATRO = 4,
+        CINCO = 5,
+        SEIS = 6,
+        SIETE = 7,
+        OCHO = 8,
+        NUEVE = 9,
+        DIEZ = 10,
+        J = 11,
+        Q = 12,
+        K = 13
+    }
+
+    class Carta
+    {
+        public Valores Valor { get; }
+        public Pintas Pinta { get; }
+
+
+        public Carta(Valores valor, Pintas pinta)
+        {
+            Valor = valor;
+            Pinta = pinta;
+        }
+
+        public override string ToString()
+        {
+            return $"{Valor} de {Pinta}";
+        }
+    }
+
+}
